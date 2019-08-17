b7370993a29cbed75f02e5a1e5e1a3a1c5a1a1e7
Luís Sousa <luiscnsousa@outlook.com>
2019-08-18 00:25:00 +0100

    Book form with client side validation

---
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,38 @@
+﻿namespace BookListRazor.Pages.BookList
+{
+    using BookListRazor.Model;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using System.Threading.Tasks;
+
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.Page();
+            }
+
+            this.db.Book.Add(this.Book);
+            await this.db.SaveChangesAsync();
+
+            return this.RedirectToPage("Index");
+        }
+    }
+}
